96897d18cb35f798a3bc2b013153971098742a17
Fernanda Namie <fernanda.namie@gmail.com>
2021-09-01 14:58:30 -0300

    adição do projeto + edição do readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# CursoRestApi
+# Curso Rest API
+
+O projeto spring presente neste repositório é resultado de um curso realizado sobre Rest API da AlgaWorks.
---
+++ workspace-spring-tool-suite/algalog-api/src/main/java/com/algaworks/algalog/domain/service/RegistroOcorrenciaService.java
@@ -0,0 +1,24 @@
+package com.algaworks.algalog.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algalog.domain.exception.NegocioException;
+import com.algaworks.algalog.domain.model.Entrega;
+import com.algaworks.algalog.domain.model.Ocorrencia;
+import com.algaworks.algalog.repository.EntregaRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class RegistroOcorrenciaService {
+	
+	private BuscaEntregaService buscaEntregaService;
+	
+	public Ocorrencia registrar(Long entregaId, String descricao) {
+		Entrega entrega = buscaEntregaService.buscar(entregaId);
+		
+		return entrega.adicionarOcorrencia(descricao);
+	}
+
+}
